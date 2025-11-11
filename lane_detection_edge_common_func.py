a126a17a80b6d791e9d7e9cc7b176f449aed3bd9
davidpark <tlsdnd001@naver.com>
2025-11-11 21:29:14 +0900

    허프변환 로직 수정

--- lane_detection_edge_common_func.py
+++ lane_detection_edge_common_func.py
@@ -19,12 +19,6 @@ def draw_lines(img, lines, color=[0, 0, 255], thickness=2):
         for x1,y1,x2,y2 in line:
             cv2.line(img, (x1, y1), (x2, y2), color, thickness)
 
-def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
-    lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
-    line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
-    draw_lines(line_img, lines)
-    return line_img
-
 def weighted_img(image, initial_image, alpha=1, beta=1., gamma=0.):
     return cv2.addWeighted(initial_image, alpha, image, beta, gamma)
 
@@ -35,7 +29,13 @@ def get_canny_image(image, low_threshold=70, high_threshold=210, kernel_size=3):
     return canny_img
 
 def get_hough_image(image, rho=1, theta=np.pi/180, threshold=30, min_line_len=10, max_line_gap=20):
+    hough_lines = get_hough_lines(image, rho, theta, threshold, min_line_len, max_line_gap)
+    hough_img = np.zeros((image.shape[0], image.shape[1], 3), dtype=np.uint8)
+    draw_lines(hough_img, hough_lines)
+    return hough_img
+
+def get_hough_lines(image, rho=1, theta=np.pi/180, threshold=30, min_line_len=10, max_line_gap=20):
     canny_img = get_canny_image(image)
     roi_img = basic_common_func.get_roi_image(canny_img)
-    hough_img = hough_lines(roi_img, rho, theta, threshold, min_line_len, max_line_gap)
-    return hough_img
+    lines = cv2.HoughLinesP(roi_img, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
+    return lines
