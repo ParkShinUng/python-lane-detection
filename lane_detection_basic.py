d46092f8bdba308b1e7ad889e7e089afdf25533f
ParkShinUng <tlsdnd001@naver.com>
2025-11-11 17:13:51 +0900

    단순 이름 변경

--- lane_detection_basic.py
+++ lane_detection_basic.py
@@ -2,12 +2,12 @@ import cv2
 import numpy as np
 import lane_detection_basic_common_func as basic_common_func
 
-image = lane_detection_basic_common_func.get_image('solidWhiteCurve.jpg')
+image = basic_common_func.get_image('solidWhiteCurve.jpg')
 
-roi_img = lane_detection_basic_common_func.get_roi_image(image, (0, 0, 255)) # vertices에 정한 점들 기준으로 ROI 이미지 생성
+roi_img = basic_common_func.get_roi_image(image, (0, 0, 255)) # vertices에 정한 점들 기준으로 ROI 이미지 생성
 copy_roi_img = np.copy(roi_img) # roi_img 복사
 
-mark_img = lane_detection_basic_common_func.get_mark_image(image, copy_roi_img) # 흰색 차선 찾기
+mark_img = basic_common_func.get_mark_image(image, copy_roi_img) # 흰색 차선 찾기
 
 # 흰색 차선 검출한 부분을 원본 image에 overlap 하기
 color_thresholds = (mark_img[:,:,0] == 0) & (mark_img[:,:,1] == 0) & (mark_img[:,:,2] > 200)
